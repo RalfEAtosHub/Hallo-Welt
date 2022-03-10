65ad980bc96d6ea2e4254e6b06df0e8b740ec2e7
RalfEAtosHub <Ralf.Engelmann@atos.net>
2022-03-10 12:19:35 +0100

    Erste Gehversuche mit EBS

--- scripts/EBSenvAbfrage.sh
+++ scripts/EBSenvAbfrage.sh
@@ -1,2 +1,6 @@
+#!bin/bash
 REenv=$(aws elasticbeanstalk describe-environments --query 'Environments[*].EnvironmentName' | grep "RalfE-Umgebung")
 echo "Ergebnis: $REenv <--"
+if [[ -z $variable ]]
+    then echo "Da ist nichts"
+fi
