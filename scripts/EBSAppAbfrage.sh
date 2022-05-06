b5e77bbfc373db7f7a3533ed6516db6165169ad9
RalfEAtosHub <Ralf.Engelmann@atos.net>
2022-05-06 11:20:04 +0200

    Troubleshooting

--- scripts/EBSAppAbfrage.sh
+++ scripts/EBSAppAbfrage.sh
@@ -1,8 +1,7 @@
 #!bin/bash
 REApp=$(aws elasticbeanstalk describe-applications --application-names | grep "Hallo-Welt")
-printf "Ergebnis: $REApp <--"
+echo "Ergebnis: $REApp <--"
 if [ -z $REApp ]
     then echo "Da ist nichts"
     else echo "$REApp"
 fi
-export REApp
